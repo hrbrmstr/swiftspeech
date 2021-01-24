6b45c200d2a1e8d02c310ea1c67b3fe3c2d0d9a1
hrbrmstr <bob@rud.is>
2021-01-24 07:32:27 -0500

    README and tests

--- inst/tinytest/test_swiftspeech.R
+++ inst/tinytest/test_swiftspeech.R
@@ -1,4 +1,7 @@
+library(swiftspeech)
 
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
+parts_of_speech("The comm wasn't working. Feeling increasingly ridiculous, he pushed the button for the 1MC channel several more times. Nothing. He opened his eyes and saw that all the lights on the panel were out. Then he turned around and saw that the lights on the refrigerator and the ovens were out. It wasn’t just the coffeemaker; the entire galley was in open revolt. Holden looked at the ship name, Rocinante, newly stenciled onto the galley wall, and said, Baby, why do you hurt me when I love you so much?") -> res
 
+expect_true(inherits(res, "data.frame"))
+
+expect_true("Preposition" %in% res$tag)
