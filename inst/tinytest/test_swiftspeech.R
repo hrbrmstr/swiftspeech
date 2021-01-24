4e6773c526ea79780827c74ee09da69de9ec6bd1
hrbrmstr <bob@rud.is>
2021-01-24 13:45:23 -0500

    cleaned up tests

--- inst/tinytest/test_swiftspeech.R
+++ inst/tinytest/test_swiftspeech.R
@@ -1,6 +1,15 @@
 library(swiftspeech)
 
-parts_of_speech("The comm wasn't working. Feeling increasingly ridiculous, he pushed the button for the 1MC channel several more times. Nothing. He opened his eyes and saw that all the lights on the panel were out. Then he turned around and saw that the lights on the refrigerator and the ovens were out. It wasn’t just the coffeemaker; the entire galley was in open revolt. Holden looked at the ship name, Rocinante, newly stenciled onto the galley wall, and said, Baby, why do you hurt me when I love you so much?") -> res
+parts_of_speech(paste0(c(
+  "The comm wasn't working. Feeling increasingly ridiculous, he pushed",
+  "the button for the 1MC channel several more times. Nothing. He opened",
+  "his eyes and saw that all the lights on the panel were out. Then he",
+  "turned around and saw that the lights on the refrigerator and the",
+  "ovens were out. It wasn’t just the coffeemaker; the entire galley was",
+  "in open revolt. Holden looked at the ship name, Rocinante, newly",
+  "stenciled onto the galley wall, and said, Baby, why do you hurt me",
+  "when I love you so much?"
+), collapse = " ")) -> res
 
 expect_true(inherits(res, "data.frame"))
 
